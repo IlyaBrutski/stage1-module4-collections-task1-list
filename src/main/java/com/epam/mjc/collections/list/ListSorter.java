30fd6223ac7f1681f24ed82d9597c5640a2c50d3
IlyaBrutski <101987935+IlyaBrutski@users.noreply.github.com>
2023-06-14 21:06:09 +0300

    List

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,15 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int x = (int) Math.pow(5 * Integer.valueOf(a), 2) + 3;
+        int y = (int) Math.pow(5 * Integer.valueOf(b), 2) + 3;
+        return x == y ? -1 : x - y;
     }
 }
